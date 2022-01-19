7b3223d28ef574721346dd147acca2c3f9e7651c
Tae-Yeon-Kim <86635960+Tae-Yeon-Kim@users.noreply.github.com>
2022-01-19 09:10:50 +0900

    Add files via upload

---
+++ 18870.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int i,a,b,c;
+	
+	scanf("%d",&a);
+	
+	for (i=0;i<a;i++){
+		//scanf("%d",&b);
+		//dam[i]=b;
+		//printf("%d ¹øÂ° dam °ª : %d\n",i,dam[i]);
+	}
+	
+}
